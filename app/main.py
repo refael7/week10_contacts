02a9163ea62d75428ec1e64d82a4bf9c91f74e23
refael_saidov <refaelsaidov1@gmail.com>
2025-12-23 09:59:15 +0200

    Fix/update main logic

---
+++ app/main.py
@@ -0,0 +1,8 @@
+class Contact:
+    def __init__(self,id, first_name, last_name, phone_number):
+        self.id=id
+        self.first_name=first_name
+        self.last_name=last_name
+        self.phone_number=phone_number
+
+    def 
