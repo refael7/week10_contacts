9c32b2f906c3dff7e31070628b64dd56a7637e3b
refael_saidov <refaelsaidov1@gmail.com>
2025-12-24 09:57:05 +0200

    add functions

--- app/data_interactor.py
+++ app/data_interactor.py
@@ -17,6 +17,14 @@ class Contact:
             "phone_number": self.phone_number
         }
 
+    def __repr__(self):
+        return (
+            f"(id={self.id}, "
+            f"first_name={self.first_name}, "
+            f"last_name={self.last_name}, "
+            f"phone_number={self.phone_number})"
+
+        )
 def get_connection():
     try:
         conn = mysql.connector.connect(
@@ -30,6 +38,67 @@ def get_connection():
     except Exception as e :
         print(f"Error connecting to database:{e}")
         return None
+
+def create_contact(first_name, last_name, phone_number) -> int:
+    conn=get_connection()
+    if conn is None:
+        return None
+    cursor = conn.cursor()
+    query = "INSERT INTO contacts (first_name, last_name, phone_number) VALUES (%s,%s,%s)"
+    cursor.execute(query,(first_name, last_name, phone_number))
+    conn.commit()
+    contact_id = cursor.lastrowid
+    cursor.close()
+    conn.close()
+    return contact_id
+
+def get_all_contacts() -> list:
+    conn = get_connection()
+    if conn is None:
+        return []
+    cursor = conn.cursor()
+    query = "SELECT id, first_name, last_name, phone_number FROM contacts"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    contacts = []
+
+    for row in rows:
+        contact = Contact(
+            id=row[0],
+            first_name=row[1],
+            last_name=row[2],
+            phone_number=row[3]
+        )
+        contacts.append(contact)
+    cursor.close()
+    conn.close()
+    return contacts
+
+def update_contact(contact_id:int, first_name:str, last_name:str, phone_number:str) -> bool:
+    conn = get_connection()
+    if conn is None:
+        return False
+    cursor = conn.cursor()
+    query = "UPDATE contacts SET first_name=%s, last_name=%s, phone_number=%s WHERE id=%s"
+    cursor.execute(query,(first_name, last_name, phone_number, contact_id))
+    conn.commit()
+    rows_affected = cursor.rowcount
+    cursor.close()
+    conn.close()
+    return rows_affected > 0
+
+def delete_contact(id) -> bool:
+    conn = get_connection()
+    if conn is None:
+        return False
+    cursor = conn.cursor()
+    query = "DELETE FROM contacts WHERE id=%s"
+    cursor.execute(query,(id,))
+    conn.commit()
+    rows_affected = cursor.rowcount
+    cursor.close()
+    conn.close()
+    return rows_affected > 0
 
 
 
